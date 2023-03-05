b46e852fa06619decc86faa67da6a619cf5a4df7
adilet524 <zhumakadyrovadilet524@gmail.com>
2023-03-05 18:25:52 +0600

    Created billitest project

---
+++ src/main/java/aksoftdev/billitest/BillitestApplication.java
@@ -0,0 +1,13 @@
+package aksoftdev.billitest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillitestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BillitestApplication.class, args);
+        System.out.println("Welcome to billitest project!");
+    }
+}
---
+++ src/test/java/aksoftdev/billitest/BillitestApplicationTests.java
@@ -0,0 +1,13 @@
+package aksoftdev.billitest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillitestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
