a9927953185e946f22484aa39d3967e10d285820
adilet524 <zhumakadyrovadilet524@gmail.com>
2023-03-07 00:00:16 +0600

    d

--- src/main/java/aksoftdev/billitest/dto/response/AuthInfoResponse.java
+++ src/main/java/aksoftdev/billitest/dto/response/AuthInfoResponse.java
@@ -1,4 +1,16 @@
 package aksoftdev.billitest.dto.response;
 
+import aksoftdev.billitest.entities.enums.Role;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class AuthInfoResponse {
+
+    private String email;
+
+    private String token;
+
+    private Role role;
 }
--- src/main/java/aksoftdev/billitest/dto/response/ClientRegisterResponse.java
+++ src/main/java/aksoftdev/billitest/dto/response/ClientRegisterResponse.java
@@ -1,4 +1,20 @@
 package aksoftdev.billitest.dto.response;
 
+import aksoftdev.billitest.entities.enums.Role;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class ClientRegisterResponse {
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String token;
+
+    private Role role;
 }
